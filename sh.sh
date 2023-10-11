d78f79937472b5adad89be95b48211c1b4aaf220
cltan023 <83808173+cltan023@users.noreply.github.com>
2023-10-11 21:02:34 +0800

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -2,7 +2,7 @@
 python main.py --seed 1 --data_name CIFAR-100 --num_classes 100 --arch resnet18 --optimizer SGD --learning_rate 0.05 --batch_size_train 128 --weight_decay 5.0e-4 --gpu_id 0 --epochs 200 --project test --scheduler cosine --use_cutmix 0
 
 # Lookahead
-python main.py --seed 1 --data_name CIFAR-100 --num_classes 100 --arch resnet18 --optimizer SGD --learning_rate 0.05 --batch_size_train 128 --weight_decay 5.0e-4 --gpu_id 0 --epochs 200 --project ablation_study --scheduler cosine --use_la 1 --k 5 --alpha 0.8 --use_cutmix 0
+python main.py --seed 1 --data_name CIFAR-100 --num_classes 100 --arch resnet18 --optimizer SGD --learning_rate 0.05 --batch_size_train 128 --weight_decay 5.0e-4 --gpu_id 0 --epochs 200 --project test --scheduler cosine --use_la 1 --k 5 --alpha 0.8 --use_cutmix 0
 
 # Sharpness-aware Minimization (SAM)
 python main.py --seed 1 --data_name CIFAR-100 --num_classes 100 --arch resnet18 --optimizer SGD --learning_rate 0.05 --batch_size_train 128 --weight_decay 5.0e-4 --gpu_id 0 --epochs 200 --project test --scheduler cosine --use_sam 1 --sam_rho 0.05 --use_cutmix 0
