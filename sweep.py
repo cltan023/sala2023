2628829877616aeaa70f5821ee8154f7a851675e
cltan023 <83808173+cltan023@users.noreply.github.com>
2023-10-16 16:26:22 +0800

    Update sweep.py

--- sweep.py
+++ sweep.py
@@ -1,14 +1,14 @@
 import wandb
 
-project = 'running_time'
+project = 'running_time' # project name
 config = {
-    'program': 'main.py',
-    'name': 'la_resnet50',
+    'program': 'main.py', # the main program that usually recieves the arguments
+    'name': 'la_resnet50', # sweep name 
     'method': 'grid',
     'metric': {'goal': 'maximize', 'name': 'best_acc'},
     'parameters': 
     {
-        'seed': {'value': 1},
+        'seed': {'value': 1}, # fixed hyper-parameter 
         'scheduler': {'value': 'cosine'},
         'data_name': {'value': 'CIFAR-100'},
         'num_classes': {'value': 100},
@@ -22,7 +22,7 @@ config = {
         'alpha': {'value': 0.8},
         'k': {'value': 5},
         # 'sala_rho': {'values': [0.1, 0.2]},
-        'learning_rate': {'values': [0.01, 0.05, 0.1]},
+        'learning_rate': {'values': [0.01, 0.05, 0.1]}, # hyper-parameter needs to be tuned
         'batch_size_train': {'value': 128},
         'weight_decay': {'values': [5.0e-4, 1.0e-3, 2.0e-3]}
      }
